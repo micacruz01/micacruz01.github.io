aa711ac94c0966ce5194a061b19742b2fabdf22c
micacruz01 <micacruz0963@gmail.com>
2024-09-15 13:32:01 +0800

    Building a Personal Web Page

---
+++ scripts.js
@@ -0,0 +1,15 @@
+
+window.onload = function() {
+    const hours = new Date().getHours();
+    let greeting;
+
+    if (hours < 12) {
+        greeting = "Good Morning!";
+    } else if (hours < 18) {
+        greeting = "Good Afternoon!";
+    } else {
+        greeting = "Good Evening!";
+    }
+
+    alert(greeting + " Welcome to my website!");
+};
